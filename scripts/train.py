66423d1f193bb93fd51a871e55a88e97a16bc9e9
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-05-20 21:35:38 +0000

    new train fn

--- scripts/train.py
+++ scripts/train.py
@@ -7,17 +7,27 @@ args = parser.parse_args()
 
 import rl4caribou
 
+## normalizing work directory 
+#
+import os
+abs_filepath = os.path.abspath(args.file)
+
+# change directory to script's directory (since io uses relative paths)
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 # training
 #
 from rl4caribou.utils import sb3_train    
-model_save_id, train_options = sb3_train(args.file, progress_bar=args.progress_bar)
+model_save_id, train_options = sb3_train(abs_filepath, progress_bar=args.progress_bar)
 
 # hugging face
 #
 if 'repo' in train_options:
     from rl4caribou.utils import upload_to_hf
     try:
-        upload_to_hf(args.file, "sb3/"+args.file, repo=train_options['repo'])
+        upload_to_hf(abs_filepath, "sb3/"+args.file, repo=train_options['repo'])
         upload_to_hf(model_save_id, "sb3/"+model_save_id+".zip", repo=train_options['repo'])
     except:
         print("Couldn't upload to hf!")
